2d82ce9fd3263815eb1f01e2f6e2f5e0b41f0c64
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-01-30 15:25:10 +0200

    Update start_emu_headless.sh

--- start_emu_headless.sh
+++ start_emu_headless.sh
@@ -29,7 +29,7 @@ function check_emulator_status () {
   printf "${G}==> ${BL}Checking device booting up status 🧐.. ${G}<==${NC}""\n"
   while [[ "$(adb shell getprop sys.boot_completed 2>&1)" != 1 ]];
   do
-    sleep 2
+    sleep 5
     if [ "$(adb shell getprop sys.boot_completed 2>&1)" == 1 ];
     then
        printf "${G}☞ ${BL}Device is fully booted and running!! 😀 : '$(adb shell getprop sys.boot_completed 2>&1)' ${G}☜${NC}""\n"
@@ -57,66 +57,78 @@ function hidden_policy() {
   adb shell "settings put global hidden_api_policy_pre_p_apps 1;settings put global hidden_api_policy_p_apps 1;settings put global hidden_api_policy 1"
 };
 
+function check_emulator_focus() {
+  echo "Android is booting..."
+  EMU_BOOTED=0
+  n=0
+  first_launcher=1
+  echo 1 > /tmp/failed
+  while [[ $EMU_BOOTED = 0 ]];do
+     echo "Test for current focus"
+
+     CURRENT_FOCUS=$(adb shell dumpsys window 2>/dev/null | grep -i mCurrentFocus)
+     echo "Current focus: ${CURRENT_FOCUS}"
+     case "${CURRENT_FOCUS}" in
+
+     *"Launcher"*)
+       if [[ "${first_launcher}" == 1 ]]; then
+         echo "Launcher seems to be ready, wait 5 sec for another popup..."
+         sleep 5
+         first_launcher=0
+       else
+         echo "Launcher is ready, Android boot completed"
+         EMU_BOOTED=1
+         rm /tmp/failed
+       fi
+     ;;
+
+#     *"Not Responding: com.android.systemui"*)
+#       echo "Dismiss System UI isn't responding alert"
+#       adb shell input keyevent KEYCODE_ENTER
+#       adb shell input keyevent KEYCODE_DPAD_DOWN
+#       adb shell input keyevent KEYCODE_ENTER
+#       first_launcher=1
+#     ;;
+
+#     *"Not Responding: com.google.android.gms"*)
+#       echo "Dismiss GMS isn't responding alert"
+#       adb shell input keyevent KEYCODE_ENTER
+#       first_launcher=1
+#     ;;
+
+#     *"Not Responding: system"*)
+#       echo "Dismiss Process system isn't responding alert"
+#       adb shell input keyevent KEYCODE_ENTER
+#       first_launcher=1
+#     ;;
+
+#     *"ConversationListActivity"*)
+#       echo "Close Messaging app"
+#       adb shell input keyevent KEYCODE_ENTER
+#       first_launcher=1
+#     ;;
+
+      *)
+        adb shell input keyevent KEYCODE_ENTER
+        adb shell input keyevent KEYCODE_DPAD_DOWN
+        adb shell input keyevent KEYCODE_ENTER
+        first_launcher=1
+        n=$((n + 1))
+        echo "Waiting for 5 sec for Android emulator to boot (${n})..."
+        sleep 5
+      ;;
+
+     esac
+  done
+  echo "Android Emulator started."
+};
+
 function access_emulator_with_adb() {
   if test -s /tmp/failed; then
     echo "Skip"
   else
     "adb" shell ls || true
   fi
-};
-
-function check_emulator_focus() {
-  EMU_BOOTED=0
-  n=0
-  first_launcher=1
-  echo 1 > /tmp/failed
-  while [[ $EMU_BOOTED = 0 ]];do
-      echo "Test for current focus"
-      #adb shell dumpsys window
-      CURRENT_FOCUS=$(adb shell dumpsys window 2>/dev/null | grep -i mCurrentFocus)
-      echo "Current focus: ${CURRENT_FOCUS}"
-      case $CURRENT_FOCUS in
-      *"Launcher"*)
-        if [[ $first_launcher == 1 ]]; then
-          echo "Launcher seems to be ready, wait 10 sec for another popup..."
-          sleep 10
-          first_launcher=0
-        else
-          echo "Launcher is ready, Android boot completed"
-          EMU_BOOTED=1
-          rm /tmp/failed
-        fi
-      ;;
-      *"Not Responding: com.android.systemui"*)
-        echo "Dismiss System UI isn't responding alert"
-        adb shell input keyevent KEYCODE_ENTER
-        adb shell input keyevent KEYCODE_DPAD_DOWN
-        adb shell input keyevent KEYCODE_ENTER
-        first_launcher=1
-      ;;
-      *"Not Responding: com.google.android.gms"*)
-        echo "Dismiss GMS isn't responding alert"
-        adb shell input keyevent KEYCODE_ENTER
-        first_launcher=1
-      ;;
-      *"Not Responding: system"*)
-        echo "Dismiss Process system isn't responding alert"
-        adb shell input keyevent KEYCODE_ENTER
-        first_launcher=1
-      ;;
-      *"ConversationListActivity"*)
-        echo "Close Messaging app"
-        adb shell input keyevent KEYCODE_ENTER
-        first_launcher=1
-      ;;
-      *)
-        n=$((n + 1))
-        echo "Waiting Android to boot 10 sec ($n)..."
-        sleep 10
-      ;;
-      esac
-  done
-  echo "Android Emulator started."
 };
 
 launch_emulator
@@ -127,7 +139,7 @@ disable_animation
 sleep 1
 hidden_policy
 sleep 1
-access_emulator_with_adb
-sleep 1
 check_emulator_focus
+sleep 1
+access_emulator_with_adb
 sleep 1
